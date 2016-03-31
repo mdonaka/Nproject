4f6db4bf4530e459a11bcf2277f1400015b635dd
ちうた <4ryo2-meron-math@ezweb.ne.jp>
2016-03-31 18:36:52 +0900

    テストコミット

--- samplegame/sample/game/game.cpp
+++ samplegame/sample/game/game.cpp
@@ -2,27 +2,47 @@
 
 
 void game(Scene *scene){
+	static int i;
+	static Graphs graph[15];
 
-	static int x = 140;
-	static int flg = 0;
-
-	SetMainWindowText("ゲーム");
-	DrawFormatString(x, 450, 0xffffff, "xでタイトルへ");
-
-	if (x <= 400 && flg == 0){
-		x += 4;
+	init(graph);
+	for (i = 0; i < 30; i++){
+		DrawExtendGraph(graph[i].x, graph[i].y, graph[i].x + graph[i].width, graph[i].y + graph[i].height, graph[i].handle, TRUE);
 	}
-	else if (x >= 140){
-		flg = 1;
-		x -= 4;
-	}
-	else{
-		flg = 0;
-	}
-
-
 	if (CheckHitKey(KEY_INPUT_X)){
-		x = 140;
 		*scene = Title;
 	}
 }
+
+void init(Graphs graph[15]){
+	/*
+	static int flg = 0;
+	if (flg == 0){
+		init_graph(graph);
+		flg = 1;
+	}
+	*/
+}
+
+void init_graph(Graphs graph[15]){
+	int i;
+	int space;
+
+	space = 10;
+
+	for (i = 0; i < 30; i++){
+		graph[i].width = 33 + (SCREEN_WIDTH - 7 * space - 33) / 6;
+		graph[i].height = (SCREEN_HEIGHT - 6 * space) / 5;
+		graph[i].x = space + (graph[i].width + space)*(i % 6);
+		graph[i].y = space + (graph[i].height + space)*(i % 5);
+		graph[i].handle = LoadGraph("image\\sample.png");
+	}
+}
+/*
+640*480
+5x+6y=480
+6x+7y+2z=640
+
+504+70=574 640-574=66
+x=84,y=10,z=33
+*/
--- samplegame/sample/game/game.h
+++ samplegame/sample/game/game.h
@@ -1,4 +1,14 @@
 #pragma once
 #include"..\\initialize\\init.h"
 
+typedef struct __Graph{
+	int handle;
+	int x;
+	int y;
+	int width;
+	int height;
+}Graphs;
+
 void game(Scene *scene);
+void init(Graphs graph[15]);
+void init_graph(Graphs graph[15]);
